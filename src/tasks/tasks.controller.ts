b8a2c97b8fe13408de6e4b4506b4992d093afe0d
Andrii Danilishin <a.i.danilishin@gmail.com>
2020-01-23 23:33:26 +0200

    createTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { Task } from "./task.model";
 
-@Controller('tasks')
+@Controller("tasks")
 export class TasksController {
-    constructor(private tasksService: TasksService){}
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks(): Task[]{
-        return this.tasksService.getAllTasks()
-    }
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body("title") title: string,
+    @Body("description") description: string
+  ) {
+      return this.tasksService.createTask(title, description)
+  }
 }
